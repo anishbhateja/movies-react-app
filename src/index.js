11e7ed059c068c61599be72d82df69c31ece4143
Anish Bhateja <anishbhateja99@gmail.com>
2020-09-27 18:36:32 +0530

    Added store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//created store, if no prev state exists, default value of state from reducer is taken as state
+const store = createStore(movies);
+console.log('store', store);
+console.log('store.getState()', store.getState());
+
+ReactDOM.render(<App />, document.getElementById('root'));
